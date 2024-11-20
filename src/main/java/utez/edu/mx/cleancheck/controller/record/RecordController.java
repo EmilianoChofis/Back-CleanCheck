0a02eed8058c437a131e27bb0df3226dd6de8771
DavidRos1231 <20213tn038@utez.edu.mx>
2024-11-19 23:56:28 -0600

    [ADD & UPDATE] Se modifico el modelo de record, y se agregaron al service y controller los metodos create y get-by-room

--- src/main/java/utez/edu/mx/cleancheck/controller/record/RecordController.java
+++ src/main/java/utez/edu/mx/cleancheck/controller/record/RecordController.java
@@ -1,4 +1,62 @@
 package utez.edu.mx.cleancheck.controller.record;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import utez.edu.mx.cleancheck.controller.record.dto.RecordDto;
+import utez.edu.mx.cleancheck.service.record.RecordService;
+import utez.edu.mx.cleancheck.utils.ApiResponse;
+import utez.edu.mx.cleancheck.model.record.Record;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api-clean/record")
+@RequiredArgsConstructor
 public class RecordController {
+
+    private final RecordService recordService;
+    //insert
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse<Record>> create(@Validated({RecordDto.Insert.class}) @RequestBody RecordDto recordDto) {
+        try {
+            ApiResponse<Record> response = recordService.insert(recordDto);
+            HttpStatus statusCode = response.isError() ? HttpStatus.BAD_REQUEST : HttpStatus.OK;
+            return new ResponseEntity<>(
+                    response,
+                    statusCode
+            );
+        } catch (Exception e) {
+            return new ResponseEntity<>(
+                    new ApiResponse<>(
+                            null, true, HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    //get by room
+    //@Josafat este conviertelo en paginado
+    @PostMapping("/get-by-room")
+    public ResponseEntity<ApiResponse<List<Record>>> getByRoom(@Validated({RecordDto.GetByRoom.class}) @RequestBody RecordDto recordDto) {
+        try {
+            ApiResponse<List<Record>> response = recordService.findByRoom(recordDto.getRoomId());
+            HttpStatus statusCode = response.isError() ? HttpStatus.BAD_REQUEST : HttpStatus.OK;
+            return new ResponseEntity<>(
+                    response,
+                    statusCode
+            );
+        } catch (Exception e) {
+            return new ResponseEntity<>(
+                    new ApiResponse<>(
+                            null, true, HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
 }
--- src/main/java/utez/edu/mx/cleancheck/controller/record/dto/RecordDto.java
+++ src/main/java/utez/edu/mx/cleancheck/controller/record/dto/RecordDto.java
@@ -1,8 +1,29 @@
 package utez.edu.mx.cleancheck.controller.record.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import utez.edu.mx.cleancheck.model.room.Room;
+import utez.edu.mx.cleancheck.model.room.RoomState;
+import utez.edu.mx.cleancheck.model.user.User;
 
 @Data
-
 public class RecordDto {
-}
+        @NotNull(groups = {RecordDto.Insert.class})
+        private RoomState previousState;
+
+        @NotNull(groups = {RecordDto.Insert.class})
+        private RoomState NewState;
+
+        @NotNull(groups = {RecordDto.Insert.class})
+        private User userId;
+
+        @NotNull(groups = {RecordDto.Insert.class, RecordDto.GetByRoom.class})
+        private Room roomId;
+
+        public interface Insert {
+        }
+
+        public interface GetByRoom {
+        }
+
+    }
