66f6c91b264b69738f0ac894d6df152f213cec5c
MisaelBD <20203tn073@utez.edu.mx>
2024-11-25 16:29:17 -0600

    Cambios PaginationType

--- src/main/java/utez/edu/mx/cleancheck/utils/PaginationType.java
+++ src/main/java/utez/edu/mx/cleancheck/utils/PaginationType.java
@@ -1,51 +1,15 @@
 package utez.edu.mx.cleancheck.utils;
 
+import lombok.Data;
+
+@Data
+
 public class PaginationType {
     private String filter;
     private int page;
     private int limit;
     private String order;
     private String sortBy;
-
-    public String getFilter() {
-        return filter;
-    }
-
-    public void setFilter(String filter) {
-        this.filter = filter;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
-
-    public int getLimit() {
-        return limit;
-    }
-
-    public void setLimit(int limit) {
-        this.limit = limit;
-    }
-
-    public String getOrder() {
-        return order;
-    }
-
-    public void setOrder(String order) {
-        this.order = order;
-    }
-
-    public String getSortBy() {
-        return sortBy;
-    }
-
-    public void setSortBy(String sortBy) {
-        this.sortBy = sortBy;
-    }
 
     @Override
     public String toString() {
